ff9132546c0de4968d3d200e01ffdc3ca557dcc9
alanserafim <alan.serafim@fatec.sp.gov.br>
2026-08-24 16:21:27 -0300

    arquivos iniciais

---
+++ backend/sispreq/src/main/java/com/cnj/sispreq/SispreqApplication.java
@@ -0,0 +1,13 @@
+package com.cnj.sispreq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispreqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SispreqApplication.class, args);
+	}
+
+}
